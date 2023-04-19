83c3f2b939e4ce4866e2f93956c10e13e6dacd64
Fraser Parlane <mail@fraserparlane.com>
2023-04-18 23:29:44 -0700

    Tile update.

--- tiles.py
+++ tiles.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 from lxml import etree, builder
 from typing import List
+import numpy as np
 
 # Initialize some SVG objects
 elements = builder.ElementMaker()
@@ -25,29 +26,86 @@ class Tile(ABC):
         self.height = height
 
         # Randomize and store the colors
-        colors = np.array(colors)
+        self._color_idx = -1
+        self.colors = np.array(colors)
         if colors_random:
-            colors = np.random.shuffle(colors)
-        self.colors = colors
+            np.random.shuffle(self.colors)
 
-        # Initialize SVG
+        # Initialize the svg
+        self.doc = None
+        self._init_svg()
+        self.build()
+
+    def get_color(self) -> str:
+        """
+        Get the next color to visualize
+        :return: color
+        """
+        self._color_idx += 1
+        return self.colors[self._color_idx % len(self.colors)]
+
+    def _init_svg(self):
+        """
+        Initialize the SVG document.
+        :return: None
+        """
+
+        # Make doc
         self.doc = svg(
             xmlns='http://www.w3.org/2000/svg',
             height=str(self.height),
             width=str(self.width),
         )
 
+        # Make background
+        self.doc.append(
+            rect(
+                x='0',
+                y='0',
+                width=str(self.width),
+                height=str(self.height),
+                fill=self.get_color(),
+            )
+        )
+
+    def save(self, filepath: str):
+        """
+        Save to disk
+        :param filepath: Where to save
+        :return: None
+        """
+        with open(filepath, 'wb') as f:
+            f.write(etree.tostring(self.doc, pretty_print=True))
+
     @abstractmethod
     def build(self):
         pass
 
 
-class CircleTile(Tile):
+class HalfCircleTile(Tile):
     def build(self):
-        return a
-
+        color = self.get_color()
+        for y in [0, self.height]:
+            self.doc.append(
+                circle(
+                    cx=str(self.width / 2),
+                    cy=str(y),
+                    r=str(self.width / 2),
+                    fill=color,
+                )
+            )
 
 
 if __name__ == '__main__':
-
-    print(a.width)
+    color_list = [
+        '#003f5c',
+        '#2f4b7c',
+        '#665191',
+        '#a05195',
+        '#d45087',
+        '#f95d6a',
+        '#ff7c43',
+        '#ffa600',
+    ]
+    tile = QuarterCircleTile(colors=color_list)
+    tile.save(filepath='test.svg')
